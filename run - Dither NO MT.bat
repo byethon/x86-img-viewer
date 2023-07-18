5a112573d2c296f594c4b949ae123ab8e3937250
Gurmehar Singh <gurmeharksp@gmail.com>
2023-07-18 12:48:34 +0530

    Add files via upload

--- run - Dither NO MT.bat
+++ run - Dither NO MT.bat
@@ -1,3 +1,23 @@
-set PATH=%PATH%;C:\Program Files (x86)\DOSBox-0.74-3
+@echo OFF
+setlocal EnableExtensions EnableDelayedExpansion
+if exist "C:\Program Files (x86)\" (
+  for /f "tokens=4" %%a in ('dir "C:\Program Files (x86)" /ad ^| findstr /i "Dosbox*"') do (
+  set Dosver=%%a
+)
+) else (
+  for /f "tokens=4" %%a in ('dir "C:\Program Files" /ad ^| findstr /i "Dosbox*"') do (
+  set Dosver=%%a
+)
+)
+if not defined Dosver goto DOSBOX_NOT_FOUND
+python -V >NUL
+if ERRORLEVEL 1 goto PYTHON_DOES_NOT_EXIST
+set PATH=%PATH%;C:\Program Files (x86)\%Dosver%
 python imgflat-dither-no-MT.py
 Dosbox.exe imgview.com
+:DOSBOX_NOT_FOUND
+echo Please install Dosbox to run
+exit
+:PYTHON_DOES_NOT_EXIST
+echo Please Install Python to continue
+exit
