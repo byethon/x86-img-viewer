e484bc64538e404f66588679664944507948fea8
Gurmehar Singh <gurmeharksp@gmail.com>
2023-07-18 10:43:26 +0530

    Direct Launch
    Windows Direct Launch Scripts

---
+++ run - Dither MT.bat
@@ -0,0 +1,3 @@
+set PATH=%PATH%;C:\Program Files (x86)\DOSBox-0.74-3
+python imgflat-dither-MT.py
+Dosbox.exe imgview.com
---
+++ run - NO DITHER.bat
@@ -0,0 +1,3 @@
+set PATH=%PATH%;C:\Program Files (x86)\DOSBox-0.74-3
+python imgflat-no-dither.py
+Dosbox.exe imgview.com
